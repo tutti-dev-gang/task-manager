fd98cd1079b8eafea85d65867dfcf9d6a5dba8f0
Alexis Yanguas <yanguasalexis@gmail.com>
2023-10-06 15:41:25 +0200

    🚀 unit-test Co-authored-by: itsjerem <itsjerem@users.noreply.github.com>

--- unit-test/math.js
+++ unit-test/math.js
@@ -18,22 +18,60 @@ const multiplyArray = (numbers) => {
 // FActorial de 60 = 8.32098711274139e+81
 
 const calculateFactorial = (n) => {
-    if (typeof n !== 'number' || n < 0) {
+    if (typeof n !== 'number' || n < 0) 
       throw new Error('Input must be a non-negative number');
-    }
-  
-    if (n === 0 || n === 1) {
+    
+    if (n === 0 || n === 1 ) {
       return 1;
     }
 
     if(n === 60) return 8.32098711274139e+81;
   
     let result = 1;
-    for (let i = 2; i <= n; i++) {
+    for (let i = 2; i <= n; i++) 
       result *= i;
-    }
+
   
     return result;
   }
 
-module.exports = { add, multiplyArray, calculateFactorial };
+
+
+const transformUsers = (users) => {
+  if (typeof users !== 'object' || users === null) {
+    throw new Error("Users must be provided.");
+  }  
+
+  users.forEach((user) => {
+    if(typeof user !== 'object' || user === null) 
+      throw new Error("Users must be provided.");
+  });
+
+  if(users.length > 3){
+    throw new Error("Too many users provided.");
+  }
+
+  // var expectedUsers = [];
+  // users.forEach((user) => {
+  //   var expectedUser = {};
+  //   expectedUser.id = user.id;
+  //   expectedUser.name = user.body.userName;
+  //   expectedUser.age = user.body.userAge;
+  //   expectedUsers.push(expectedUser);
+  // });
+  // return expectedUsers;
+
+
+   return users.map((user) => {
+    return {
+      id: user.id,
+      name: user.body.userName,
+      age: user.body.userAge,
+    };
+  });
+
+
+}
+
+module.exports = { add, multiplyArray, calculateFactorial, transformUsers };
+
--- unit-test/math.test.js
+++ unit-test/math.test.js
@@ -82,3 +82,76 @@ describe("Calculate Factorial Function", () => {
     });
 
 });
+
+
+const { transformUsers } = require("./math");
+
+const users = [
+  {
+    id: 0,
+    body: {
+      userName: "julien",
+      userAge: 23,
+    },
+  },
+  {
+    id: 1,
+    body: {
+      userName: "adrien",
+      userAge: 58,
+    },
+  },
+  {
+    id: 2,
+    body: {
+      userName: "jules",
+      userAge: 7,
+    },
+  },
+];
+
+const expectedUsers = [
+  {
+    id: 0,
+    name: "julien",
+    age: 23,
+  },
+  {
+    id: 1,
+    name: "adrien",
+    age: 58,
+  },
+  {
+    id: 2,
+    name: "jules",
+    age: 7,
+  },
+];
+
+const tooManyUsers = users.concat(users).concat(users);
+
+describe("transform User", () => {
+  it("should throw if no users provided", () => {
+    console.log(typeof users);
+    expect(() => transformUsers(123)).toThrow("Users must be provided.");
+    expect(() => transformUsers("salut")).toThrow("Users must be provided.");
+    expect(() => transformUsers(undefined)).toThrow("Users must be provided.");
+    expect(() => transformUsers(null)).toThrow("Users must be provided.");
+  });
+
+  it('should throw if users is an array', () => {
+    expect(() => transformUsers(["user1", "user2"])).toThrow(
+      "Users must be provided."
+    );
+  });
+
+  it("should throw if too manyUsers provided", () => {
+    expect(() => transformUsers(tooManyUsers)).toThrow(
+      "Too many users provided."
+    );
+  });
+
+  it("should transform users to the good model", () => {
+    expect(transformUsers(users)).toStrictEqual(expectedUsers);
+  });
+});
